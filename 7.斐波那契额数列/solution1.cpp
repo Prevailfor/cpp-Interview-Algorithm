4ccf0b85e5865769b3d43c01c821636e3a134e35
Prevailfor <1150197492@qq.com>
2019-04-03 17:43:44 +0800

    Create solution1.cpp

---
+++ 7.斐波那契额数列/solution1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // f(n) = f(n-1) + f(n-2): imply that this can use recursion, but have too many repeat calculate
+    // repeat calculate can use memory table to avoid, but in this problem that is not wisdom. 
+    int Fibonacci(int n) {
+        int a = 1;
+        int b = 1;
+        if(n == 0)
+            return 0;
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        int i = 3;
+        int c;
+        while(i <= n){
+            c = a + b;
+            a = b;
+            b = c;
+            i++;
+        }
+        return c;
+    }
+};
