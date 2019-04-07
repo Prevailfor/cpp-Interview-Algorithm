e65aca2d5c05091dba5daf311ee11056a1833467
Prevailfor <1150197492@qq.com>
2019-04-07 19:56:46 +0800

    Create solution2.cpp

---
+++ 24.二叉树中和为某一值的路径/solution2.cpp
@@ -0,0 +1,37 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    vector<vector<int> > res;
+    vector<int> tmp;
+    vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
+        if(root == NULL)
+            return res;
+        dfs(root, expectNumber);
+        return res;
+    }
+    
+    void dfs(TreeNode* root, int target){ // root must not be NULL.
+        tmp.push_back(root->val);
+        int tsub = target - root->val;
+        
+        if(root->left==NULL && root->right==NULL && tsub){ // leaf node
+            res.push_back(tmp);
+            return;
+        }
+        if(root->left!=NULL)
+            dfs(root->left, tsub);
+        if(root->right!=NULL)
+            dfs(root->right, tsub);
+        
+        tmp.pop_back();
+    }
+    
+};
